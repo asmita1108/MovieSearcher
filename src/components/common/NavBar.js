162d1aed06f1ee381e47514bc0ea248217d663b2
asmita1108 <asmita.bhumca17@gmail.com>
2020-09-17 15:12:07 +0530

    indent of navbar

--- src/components/common/NavBar.js
+++ src/components/common/NavBar.js
@@ -3,7 +3,7 @@ import React from "react";
 const NavBar = () => {
   return (
     <nav>
-      <div className="nav-wrapper container">
+      <div className="nav-wrapper container ">
         <a href="#/" className="brand-logo">
           {" "}
           Movie Searcher
